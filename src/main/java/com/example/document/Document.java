c5559097f4e3efcc1491d2deec36ff0e74f02814
muhammad <69121442+muhammadihabk@users.noreply.github.com>
2023-01-11 16:22:57 +0200

    Implemented backend of the Flesch score feature

--- src/main/java/com/example/document/Document.java
+++ src/main/java/com/example/document/Document.java
@@ -6,28 +6,69 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Document {
-    private String document;
+    private String text;
+    private int wordsCount = 0;
+    private int sentencesCount = 0;
+    private int syllablesCount = 0;
     
-    public Document(String document) {
-        this.document = document;
+    public Document(String text) {
+        this.text = text;
     }
 
-    public static void main(String[] args) {
-        String simpleDoc = "Hello! This is Muhammad. Abc, abc abc abc.";
-        Document document = new Document(simpleDoc);
-        document.getFleschScore(simpleDoc);
+    public double getFleschScore() {
+        List<String> tokens = getTokens();
+        for(String token : tokens) {
+            if(isWord(token)) {
+                wordsCount++;
+            } else {
+                sentencesCount++;
+            }
+            syllablesCount += countSyllables(token);
+        }
+        if(isWord(tokens.get(tokens.size() - 1))) {
+            sentencesCount++;
+        }
+        
+        return 206.835 - 1.015 * ((double) wordsCount / sentencesCount) - 84.6 * ((double) syllablesCount / wordsCount);
     }
 
-    public double getFleschScore(String document) {
-        return 0;
+    public void setText(String text) {
+        this.text = text;
+        wordsCount = 0;
+        sentencesCount = 0;
+        syllablesCount = 0;
+    }
+
+    private int countSyllables(String word) {
+        word = word.toLowerCase();
+        String characters = "aeiouy";
+        int count = 0;
+        for(int i = 0; i < word.length(); i++) {
+            if(characters.contains(String.valueOf(word.charAt(i)))) {
+                while(i < word.length() && characters.contains(String.valueOf(word.charAt(i)))) { i++; }
+                count++;
+            }
+        }
+        if(word.charAt(word.length() - 1) == 'e') {
+            if(count == 1) {
+                return 1;
+            } else {
+                count--;
+            }
+        }
+        return count;
     }
 
     private List<String> getTokens() {
         List<String> tokens = new LinkedList<>();
-        Matcher matcher = Pattern.compile("[a-zA-Z]+|[.!?]+").matcher(document);
+        Matcher matcher = Pattern.compile("[a-zA-Z]+|[.!?]+").matcher(text);
         while(matcher.find()) {
             tokens.add(matcher.group());
         }
         return tokens;
+    }
+
+    private boolean isWord(String text) {
+        return ".!?".indexOf(text.charAt(0)) == -1;
     }
 }
--- src/test/java/com/example/DocumentTest.java
+++ src/test/java/com/example/DocumentTest.java
@@ -11,14 +11,44 @@ import com.example.document.Document;
 public class DocumentTest {
     private Document document;
     private String simpleDoc;
+    private String simpleDoc_2;
+    private String syllablesEndsE_1;
+    private String syllablesEndsE_2;
+    private String syllablesContinous_1;
+    private String syllablesContinous_2;
 
     @BeforeEach
     public void setup() {
         document = new Document(simpleDoc);
-        simpleDoc = "Hello! This is Muhammad. Abc, abc abc abc.";
+        simpleDoc = "Hello!!! This is Muhammad... Abc, abc abc abc.";
+        simpleDoc_2 = "Hello!!! This is Muhammad... Abc, abc abc abc";
+        syllablesEndsE_1 = "the";
+        syllablesEndsE_2 = "plane";
+        syllablesContinous_1 = "bean";
+        syllablesContinous_2 = "heelloo";
+
     }
 
     @Test
-    public void getNumWordsAndSentences() {
+    public void countWordsSentencesAndSyllables() {
+        // document.setText(simpleDoc);
+        // document.getFleschScore();
+        // assertEquals(8, document.getWordsCount(), "Count of words");
+        // assertEquals(3, document.getSentencesCount(), "Count of sentences");
+        // assertEquals(11, document.getSyllablesCount(), "Count of syllables");
+        
+        // document.setText(simpleDoc_2);
+        // document.getFleschScore();
+        // assertEquals(8, document.getWordsCount(), "Count of words");
+        // assertEquals(3, document.getSentencesCount(), "Count of sentences");
+        // assertEquals(11, document.getSyllablesCount(), "Count of syllables");
+    }
+    
+    @Test
+    public void countSyllables() {
+        // assertEquals(1, document.countSyllables(syllablesEndsE_1), "Syllables");
+        // assertEquals(1, document.countSyllables(syllablesEndsE_2), "Syllables");
+        // assertEquals(1, document.countSyllables(syllablesContinous_1), "Syllables");
+        // assertEquals(2, document.countSyllables(syllablesContinous_2), "Syllables");
     }
 }
